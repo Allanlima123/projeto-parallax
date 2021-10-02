3f51fad54fee631dab36875e1d877bfcac30f240
alan lima <alan.silva1f@gmail.com>
2021-10-02 20:29:41 -0300

    commit do projeto parallax

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const array = ["123", "456"];
+
+obtemComprimentoDoArray();
+
+function obtemComprimentoDoArray(){
+    return array.length;
+}
+
+import moduleName from 'app';
+
+setTimeout(() => {
+     limankjkjdljdjkdjfjdjffkdjkjffk
+}, 1000);
